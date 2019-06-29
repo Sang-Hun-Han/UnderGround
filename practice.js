2cebc54de6abba7298e7e3142d7cc90607fb9def
Sang Hun <eric7477@naver.com>
2019-06-29 17:27:36 +0900

    practice1

---
+++ practice.js
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a,b;
+        a= sc.nextInt();
+        b= sc.nextInt();
+        if(a>0 && b<10){
+        System.out.println(a+b);
+        }
+    }
+}
